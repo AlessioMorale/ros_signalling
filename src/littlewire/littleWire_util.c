649892fd72cacfee1945bc94bc4eba15e0cf6ebd
Alessio Morale <alessiomorale@gmail.com>
2019-09-21 01:05:37 +0100

    Initial Version: Just change leds colour according to the received Blink messages

---
+++ src/littlewire/littleWire_util.c
@@ -0,0 +1,11 @@
+
+#include <littleWire_util.h>
+
+/* Delay in miliseconds */
+void delay(unsigned int duration) {
+#ifdef __linux__
+  usleep(duration * 1000);
+#else
+  Sleep(duration);
+#endif
+}
